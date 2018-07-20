1565bb50dcd2d8addaed3da63578d535fd69ff79
Cristi <cristicismas99@gmail.com>
2018-07-20 19:06:21 +0300

    Fix navigation to actually navigate

--- index.js
+++ index.js
@@ -9,4 +9,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
             });
         });
     });
+
+    const checkbox = document.getElementById('nav-toggle');
+
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            document.getElementsByClassName('navigation__background')[0].style.transform = 'scale(100)';
+
+            document.getElementsByClassName('navigation__nav')[0].style.opacity = '1';
+            document.getElementsByClassName('navigation__nav')[0].style.width = '100%';
+        } else {
+            document.getElementsByClassName('navigation__background')[0].style.transform = 'scale(0)';
+
+            document.getElementsByClassName('navigation__nav')[0].style.opacity = '0';
+            document.getElementsByClassName('navigation__nav')[0].style.width = '0';
+        }
+    });
+
+    // Close overlay on link click
+    const navLinks = document.getElementsByClassName('navigation__link');
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', function(e) {
+            checkbox.checked = false;
+
+            document.getElementsByClassName('navigation__background')[0].style.transform = 'scale(0)';
+
+            document.getElementsByClassName('navigation__nav')[0].style.opacity = '0';
+            document.getElementsByClassName('navigation__nav')[0].style.width = '0';
+        });
+    }
 });
